a975b0d656dc69d964f2ac1690aa2d5572d19ab4
Khaled Hamad <88118291+knhamad@users.noreply.github.com>
2022-02-14 11:30:31 -0500

    Create Code.py

---
+++ Code.py
@@ -0,0 +1,16 @@
+import cv2          
+import matplotlib.pyplot as plt
+#from tensorflow.keras.preprocessing import image
+# path to input image is specified and
+# image is loaded with imread command
+image = cv2.imread('Image file')
+# cv2.cvtColor is applied over the
+# image input with applied parameters
+# to convert the image in grayscale
+Grey_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# applying Otsu thresholding
+# as an extra flag in binary 
+# thresholding     
+ret, Segmented_img = cv2.threshold(Grey_img, 1, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)     
+plt.imshow(Segmented_img)
+plt.show()
